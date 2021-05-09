d54057294afe05a9bc00c1ee1423d19d6e325668
lucassl2020 <lucaslopes072013@gmail.com>
2021-05-09 02:28:35 -0300

    Alterações no código teste.py e remoção de uma pasta

--- pyqt5/teste.py
+++ pyqt5/teste.py
@@ -1,39 +1,15 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
 import sys
 
-class Ui_Form(object):
-    def setupUi(self, Form):
-        Form.setObjectName("Form")
-        Form.resize(656, 494)
-        Form.setStyleSheet("background-color: rgba(255, 255, 255, 170);")
-        self.label = QtWidgets.QLabel(Form)
-        self.label.setGeometry(QtCore.QRect(110, 40, 411, 411))
-        self.label.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:1, x2:0, y2:0, stop:0 rgba(255, 255, 127, 130), stop:0.935 rgba(37, 37, 37, 130));\n"
-"border-radius:10px;\n"
-"")
-        self.label.setText("")
-        self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(Form)
-        self.label_2.setGeometry(QtCore.QRect(250, 0, 131, 51))
-        self.label_2.setStyleSheet("font: 75 italic 10pt \"Comic Sans MS\";\n"
-"")
-        self.label_2.setMidLineWidth(-1)
-        self.label_2.setScaledContents(False)
-        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_2.setObjectName("label_2")
-
-        self.retranslateUi(Form)
-        QtCore.QMetaObject.connectSlotsByName(Form)
-
-    def retranslateUi(self, Form):
-        _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
-        self.label_2.setText(_translate("Form", "Lucas Silva Lopes"))
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    Form = QtWidgets.QWidget()
-    ui = Ui_Form()
-    ui.setupUi(Form)
-    Form.show()
-    sys.exit(app.exec_())
+class Window(QWidget):
+	def __init__(self, parent=none):
+		super().__init__(parent)
+		self.setWindowTitle(“Hello World”)
+		self.resize(800, 600)
+		#self.setWIndowIcon(QIcon(“/static/logo.ico”))
+		
+if __name__ == '__main__':
+	root = QApplication(sys.argv)
+	app = Window()
+	app.show()
+	sys.exit(root.exec_())
