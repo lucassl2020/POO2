ca6faf4b2cfdffd8f6207fa1c3eaad5ba53aec30
lucassl2020 <lucaslopes072013@gmail.com>
2021-05-11 04:45:48 -0300

    Criando janela com uma barra de progresso

---
+++ pyqt5/BarraDeProgresso.py
@@ -0,0 +1,33 @@
+from PyQt5.QtWidgets import QWidget, QProgressBar, QApplication
+from PyQt5.QtCore import QBasicTimer
+import sys
+
+class Window(QWidget):
+	def __init__(self, parent=None):
+		super(Window, self).__init__()
+		self.set_setting()
+		self.create_widgets()
+
+	def set_setting(self):
+		self.resize(350, 200)
+
+	def create_widgets(self):
+		self.progress_bar = QProgressBar(self)
+		self.progress_bar.setFixedWidth(300)
+		self.progress_bar.move(30, 80)#Mover posição na tela (X, Y)
+		#timer creating
+		self.timer = QBasicTimer()
+		self.step = 0
+		self.timer.start(100, self)
+
+	def timerEvent(self, e):
+		if self.step >= 100:
+			self.timer.stop()
+		self.step += 1
+		self.progress_bar.setValue(self.step)
+
+if __name__ == '__main__':
+	root = QApplication([])
+	app = Window()
+	app.show()
+	sys.exit(root.exec_())
