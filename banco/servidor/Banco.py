3d88d353956bcfe91d267fe7edbcc267ad22df7c
lucassl2020 <lucaslopes072013@gmail.com>
2021-05-23 17:34:59 -0300

    Object Literals

--- banco/servidor/Banco.py
+++ banco/servidor/Banco.py
@@ -7,6 +7,8 @@ class Banco():
 	def __init__(self):
 		self._clientes = {}
 		self._contas = {}
+		self.requests = {}
+		self.create_requests()
 		self.create_server()
 		self.start_server()
 
@@ -28,80 +30,40 @@ class Banco():
 		self.server_socket.bind(self.address)
 		self.server_socket.listen(10)
 
+	def adicionar_request(self, request_name, request_func):
+		self.requests[request_name] = request_func
+
+	def create_requests(self):
+		self.adicionar_request("cadastrar_cliente", self.cadastrarCliente)
+		self.adicionar_request("login", self.login)
+		self.adicionar_request("get_titular", self.getTitular)
+		self.adicionar_request("get_saldo", self.getSaldo)
+		self.adicionar_request("get_limite", self.getLimite)
+		self.adicionar_request("get_historico", self.getHistorico)
+		self.adicionar_request("get_titular_cpf_destinatario", self.getTitularCpfDestinatario)
+		self.adicionar_request("sacar", self.sacar)
+		self.adicionar_request("depositar", self.depositar)
+		self.adicionar_request("transferir", self.transferir)
+
+	def requestReceber(self):
+		request = self.con.recv(1024).decode()
+
+		request_list = request.split(",")
+
+		return request_list[0], request_list[1:]
+
 	def start_server(self):
 		print("START SERVER...")
 
 		self.con, self.cliente = self.server_socket.accept()
 
 		while(True):
-			self.recebe = self.requestReceber()
-
-			print("RUNNING SERVER...")
-
-			resposta = "False"
+			request_name, request_parameters = self.requestReceber()
 			
 			try:
-				if self.recebe[0] == "cadastrar_cliente":
-					cliente = Cliente(self.recebe[1], self.recebe[2], self.recebe[3])
-					if self.clienteCadastrar(cliente):
-						if self.contaCadastrar(Conta(cliente, self.recebe[4])):
-							resposta = "True"
-							resposta += ","
-							resposta += str(Conta.qtd_de_contas())
-
-				elif self.recebe[0] == "login":
-					resposta = str(self.login(self.recebe[1], self.recebe[2]))
-
-				elif self.recebe[0] == "get_titular":
-					if self.contas[self.recebe[1]].senha == self.recebe[2]:
-						resposta = "True"
-						resposta += ','
-						resposta += self.contas[self.recebe[1]].titular.nome
-						resposta += ' '
-						resposta += self.contas[self.recebe[1]].titular.sobrenome			
-
-				elif self.recebe[0] == "get_saldo":
-					if self.contas[self.recebe[1]].senha == self.recebe[2]:
-						resposta = "True"
-						resposta += ','
-						resposta += str(self.contas[self.recebe[1]].saldo)
+				func = self.requests[request_name]
 				
-				elif self.recebe[0] == "get_limite":
-					if self.contas[self.recebe[1]].senha == self.recebe[2]:
-						resposta = 'True'
-						resposta += ','
-						resposta += str(self.contas[self.recebe[1]].limite)
-				
-				elif self.recebe[0] == "get_titular_cpf_destinatario":
-					try:
-						if self.contas[self.recebe[1]].senha == self.recebe[2]:
-							resposta = "True"
-							resposta += ","
-							resposta += self.contas[self.recebe[3]].titular.nome
-							resposta += ' '
-							resposta += self.contas[self.recebe[3]].titular.sobrenome
-							resposta += ','
-							resposta += self.contas[self.recebe[3]].titular.cpf[0:3]
-					except:
-						resposta = "False"
-
-				elif self.recebe[0] == "sacar":
-					if self.contas[self.recebe[1]].senha == self.recebe[2]:
-						resposta = str(self.contas[self.recebe[1]].saca(float(self.recebe[3])))
-
-				elif self.recebe[0] == "depositar":
-					if self.contas[self.recebe[1]].senha == self.recebe[2]:
-						resposta = str(self.contas[self.recebe[1]].deposita(float(self.recebe[3])))
-
-				elif self.recebe[0] == "transferir":
-					if self.contas[self.recebe[1]].senha == self.recebe[2]:
-						resposta = str(self.contas[self.recebe[1]].transfere(self.contas[self.recebe[3]], float(self.recebe[4])))
-				
-				elif self.recebe[0] == "get_historico":
-					if self.contas[self.recebe[1]].senha == self.recebe[2]:
-						resposta = "True"
-						resposta += ","
-						resposta += self.contas[self.recebe[1]].extrato()
+				resposta = func(request_parameters)
 			except:
 				break
 
@@ -109,34 +71,139 @@ class Banco():
 
 		self.server_socket.close()
 
-	def requestReceber(self):
-		request = self.con.recv(1024).decode()
+	def cadastrarCliente(self, request_parameters):
+		print("Cadastrando cliente")
 
-		return request.split(",")
+		resposta = 'False'
 
-	def clienteCadastrar(self, cliente):
+		cliente = Cliente(request_parameters[0], request_parameters[1], request_parameters[2])
+
+		if cliente.cpf not in self.clientes and cliente.cpf.isdigit() and len(cliente.cpf) == 11:
+			self.clientes[cliente.cpf] = cliente
+							
+			if self.cadastrarConta(Conta(cliente, request_parameters[3])):
+				resposta = "True"
+				resposta += ","
+				resposta += str(Conta.qtd_de_contas())
+
+		return resposta
+
+	def cadastrarConta(self, conta):
+		print("Cadastrando conta")
+
+		self.contas[conta.numero] = conta
+		return True
+
+	def login(self, request_parameters):
+		print("Verificando login")
+
+		resposta = "False"
+
+		if self.contas[request_parameters[0]].senha == request_parameters[1]:
+			resposta = "True"
+
+		return resposta
+
+	def getTitular(self, request_parameters):
+		print("Buscando e retornando titular")
+
+		resposta = "False"
+
+		if self.contas[request_parameters[0]].senha == request_parameters[1]:
+			resposta = "True"
+			resposta += ','
+			resposta += self.contas[request_parameters[0]].titular.nome
+			resposta += ' '
+			resposta += self.contas[request_parameters[0]].titular.sobrenome
+
+		return resposta
+
+	def getSaldo(self, request_parameters):
+		print("Buscando e retornando saldo")
+
+		resposta = "False"
+
+		if self.contas[request_parameters[0]].senha == request_parameters[1]:
+			resposta = "True"
+			resposta += ','
+			resposta += str(self.contas[request_parameters[0]].saldo)
+
+		return resposta
+
+	def getLimite(self, request_parameters):
+		print("Buscando e retornando limite")
+
+		resposta = "False"
+
+		if self.contas[request_parameters[0]].senha == request_parameters[1]:
+			resposta = 'True'
+			resposta += ','
+			resposta += str(self.contas[request_parameters[0]].limite)
+
+		return resposta
+
+	def getHistorico(self, request_parameters):
+		print("Buscando e retornando historico")
+
+		resposta = "False"
+
+		if self.contas[request_parameters[0]].senha == request_parameters[1]:
+			resposta = "True"
+			resposta += ","
+			resposta += self.contas[request_parameters[0]].extrato()
+
+		return resposta
+
+	def getTitularCpfDestinatario(self, request_parameters):
+		print("Buscando e retornando titular e cpf do destinatario")
+
+		resposta = "False"
+
 		try:
-			if cliente.cpf not in self.clientes and cliente.cpf.isdigit() and len(cliente.cpf) == 11:
-				self.clientes[cliente.cpf] = cliente
-				return True
-			return False
+			if self.contas[request_parameters[0]].senha == request_parameters[1]:
+				resposta = "True"
+				resposta += ","
+				resposta += self.contas[request_parameters[2]].titular.nome
+				resposta += ' '
+				resposta += self.contas[request_parameters[2]].titular.sobrenome
+				resposta += ','
+				resposta += self.contas[request_parameters[2]].titular.cpf[0:3]
 		except:
-			return False 
+			pass
 
-	def contaCadastrar(self, conta):
-		try:
-			self.contas[conta.numero] = conta
-			return True
-		except:
-			return False 
+		return resposta
 
-	def login(self, numero_da_conta, senha):
+	def sacar(self, request_parameters):
+		print("Realizando saque")
+
+		resposta = "False"
+
+		if self.contas[request_parameters[0]].senha == request_parameters[1]:
+			resposta = str(self.contas[request_parameters[0]].saca(float(request_parameters[2])))
+
+		return resposta
+
+	def depositar(self, request_parameters):
+		print("Realizando deposito")
+
+		resposta = "False"
+
+		if self.contas[request_parameters[0]].senha == request_parameters[1]:
+			resposta = str(self.contas[request_parameters[0]].deposita(float(request_parameters[2])))
+
+		return resposta
+
+	def transferir(self, request_parameters):
+		print("Realizando transferencia")
+
+		resposta = "False"
 		try:
-			if self.contas[numero_da_conta].senha == senha:
-				return True
-			return False
+			if self.contas[request_parameters[0]].senha == request_parameters[1]:
+				resposta = str(self.contas[request_parameters[0]].transfere(self.contas[request_parameters[2]], float(request_parameters[3])))
 		except:
-			return False
+			pass
+
+		return resposta
 
 
 if __name__ == "__main__":
