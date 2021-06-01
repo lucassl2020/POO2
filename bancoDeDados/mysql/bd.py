69aa1f5e073ada9334bad1093f7a7c02659f573c
lucassl2020 <lucaslopes072013@gmail.com>
2021-06-01 16:29:59 -0300

    Banco de dados com python

---
+++ bancoDeDados/mysql/bd.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+conexao = sqlite3.connect("testDB.sqlite")
+cursor = conexao.cursor()
+
+sql = """CREATE TABLE IF NOT EXISTS usuarios(id integer PRIMARY KEY,
+         nome text NOT NULL,email text NOT NULL);"""
+
+nome = "lucas"
+email = "lucaslopes@gmail.com"
+
+cursor.execute(sql)
+for i in range(5):
+    cursor.execute("INSERT INTO usuarios (nome, email) VALUES (?,?)", (nome, email))
+
+cursor.execute("SELECT * from usuarios")
+
+for c in cursor:
+    print(c)
+
+conexao.commit()
+conexao.close()
---
+++ bancoDeDados/sqlite/bd_mysql.py
@@ -0,0 +1,23 @@
+import mysql.connector as mysql
+
+conexao = mysql.connect(host = "localhost", db="testDB", user="root", passwd="")
+cursor = conexao.cursor()
+
+sql = """CREATE TABLE IF NOT EXISTS usuarios(id integer AUTO_INCREMENT PRIMARY KEY,
+         nome text NOT NULL,email text NOT NULL);"""
+
+nome = "lucas"
+email = "lucaslopes@gmail.com"
+
+cursor.execute(sql)
+for i in range(5):
+    cursor.execute("INSERT INTO usuarios (nome, email) VALUES (%s,%s)", (nome, email))
+
+cursor.execute("SELECT * from usuarios")
+
+for c in cursor:
+    print(c)
+
+conexao.commit()
+conexao.close()
+
